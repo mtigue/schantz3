7fc2db86c06c75c55a8ac0de98ea3f27ea5892ef
Michael Tigue <mtigue@web4uonline.com>
2013-01-04 18:25:08 -0500

    8

---
+++ _vti_cnf/get_form.php
@@ -0,0 +1,21 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timelastmodified:TR|04 Jan 2013 23:24:42 -0000
+vti_timecreated:TR|04 Jan 2013 23:20:31 -0000
+vti_title:SR|Untitled 1
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|04 Jan 2013 23:23:52 -0000
+vti_syncofs_208.109.14.76\:21/schantz:TW|04 Jan 2013 23:24:48 -0000
+vti_syncwith_208.109.14.76\:21/schantz:TX|04 Jan 2013 23:24:42 -0000
+vti_cacheddtm:TX|04 Jan 2013 23:24:42 -0000
+vti_filesize:IR|520
+vti_cachedtitle:SR|Untitled 1
+vti_cachedbodystyle:SR|<body>
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
